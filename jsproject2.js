df9528091b72216fbfe6f84c695521368e374e10
Lila92T <codemyway25@outlook.com>
2026-05-15 17:27:06 +0100

    Add fibonacci function to calculate Fibonacci sequence

--- jsproject2.js
+++ jsproject2.js
@@ -57,3 +57,12 @@ function isPrime(n) {
 }
 console.log(isPrime(11));
 console.log(isPrime(12));
+
+function fibonacci(n) {
+  let fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib;
+}
+console.log(fibonacci(6));
